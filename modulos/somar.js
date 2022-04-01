66c8f20f2081754b62ddb90396948d81c9705dd0
unknown <selaualvesguilherme@gmail.com>
2022-04-01 08:43:05 -0300

    1.0.0

---
+++ modulos/somar.js
@@ -0,0 +1,14 @@
+var somar = function (x,y) {
+  return x + y;
+};
+module. exports = somar;
+
+var multiplicar = function (x,y) {
+  return x * y;
+};
+module. exports = multiplicar;
+
+var dividir = function (x,y) {
+  return x * y;
+};
+module. exports = dividir;
